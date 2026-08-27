2a7304e21ef4c3a5ec15e743b1f5f9355acbc187
NCLAN\etsolako <eleni.tsolakou@netcompany.com>
2026-08-27 10:31:13 +0300

    chore: remove unsuccessful Pages workflow

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-import process from "node:process";
-
-const basePath = process.env.PAGES_BASE_PATH ?? "";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath || undefined,
   images: {
     unoptimized: true,
   },
